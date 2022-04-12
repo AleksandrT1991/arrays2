e98b731f2646e266a5267aed27fb4de8ba3aefe7
AleksandrTsygulev <aleksandr.tsygulev@gmail.com>
2022-04-12 20:35:17 +0300

    на проверку

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -50,7 +50,6 @@ import java.util.Random;
                 char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
                 for (int i = reverseFullName.length-1; i >=0; i--)
                     System.out.print(reverseFullName[i]);
-                    System.out.println();
 
                 }
            }
